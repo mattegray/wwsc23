f0a93cfdff3002dba9f6bc8e505a3dc8e315ffe0
Matthew Kwon <hyungwoo_128@outlook.com>
2023-06-11 20:18:07 +0900

    Enable NEXT_PUBLIC_APP_URL

--- env.mjs
+++ env.mjs
@@ -3,9 +3,9 @@ import { z } from "zod"
 
 export const env = createEnv({
   client: {
-    // NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   runtimeEnv: {
-    // NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 })
