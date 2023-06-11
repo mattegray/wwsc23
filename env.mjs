2efcb66659e4a3650a5255cb7cbf2f3f29940519
Matthew Kwon <hyungwoo_128@outlook.com>
2023-06-11 20:23:33 +0900

    Add server:

--- env.mjs
+++ env.mjs
@@ -2,6 +2,10 @@ import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
 export const env = createEnv({
+  server: {
+    // This is optional because it's only used in development.
+    // See https://next-auth.js.org/deployment.
+  },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
