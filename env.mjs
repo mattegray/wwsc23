4b53c2e9b5c25c66e4a5dfa17a996863af429763
Matthew Kwon <hyungwoo_128@outlook.com>
2023-06-11 20:40:56 +0900

    Add NEXTAUTH_URL

--- env.mjs
+++ env.mjs
@@ -5,12 +5,13 @@ export const env = createEnv({
   server: {
     // This is optional because it's only used in development.
     // See https://next-auth.js.org/deployment.
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXTAUTH_URL: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   runtimeEnv: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 })
