6ab5b6ae395e93229d452546e4823c7fef556924
Matthew Kwon <hyungwoo_128@outlook.com>
2023-06-15 00:18:59 +0900

    Add Beyond We Will page

--- app/(marketing)/beyond/page.tsx
+++ app/(marketing)/beyond/page.tsx
@@ -1,7 +1,7 @@
 import { WIP } from "@/components/wip"
 
 export const metadata = {
-  title: "Sessions",
+  title: "Beyond We Will",
 }
 
 export default async function SessionPage() {
@@ -10,10 +10,10 @@ export default async function SessionPage() {
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">
-            Sessions
+            Beyond We Will
           </h1>
           <p className="text-lg text-muted-foreground sm:text-xl">
-            엘더들이 준비한 특강을 선택해서 들어보세요.
+            We Will 이후에 진행되는 챌린지들을 통해 나의 성장의 기회를 발견해 보세요.
           </p>
         </div>
       </div>
