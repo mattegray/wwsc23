83f0a66e6ccc5c9a16ba6c9ac8f5420b287485c3
Matthew Kwon <hyungwoo_128@outlook.com>
2023-06-15 18:20:48 +0900

    Change WIP message to Under Construction

--- components/wip.tsx
+++ components/wip.tsx
@@ -4,8 +4,7 @@ export function WIP() {
   return (
     <div className="text-center">
       <h1 className="text-xl font-bold sm:text-3xl">
-        <Icons.microwave size={36} className="mr-4 inline"/>
-        Still baking...
+        🚧 Under Construction...
       </h1>
     </div>
   )
