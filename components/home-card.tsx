26ca3be7e8cb3191afb7e1c0254113c388088f96
Matthew Kwon <hyungwoo_128@outlook.com>
2023-06-25 04:04:10 +0900

    Adjust HomeCard component

--- components/home-card.tsx
+++ components/home-card.tsx
@@ -12,11 +12,11 @@ export function HomeCard({ href, icon, heading, description }: HomeCardProps) {
   return (
     <Link href={href}>
       <div className="relative overflow-hidden rounded-lg border bg-background p-2 hover:shadow-xl">
-        <div className="flex h-[160px] flex-row rounded-md p-8">
-          {icon}
+        <div className="flex flex-row rounded-md p-6 sm:h-auto md:h-[180px]">
+          <div className="w-8" >{icon}</div>
           <div className="space-y-2 pl-6">
-            <h2 className="text-2xl font-bold">{heading}</h2>
-            <p className="text-lg text-muted-foreground">{description}</p>
+            <h2 className="text-3xl font-bold md:text-4xl">{heading}</h2>
+            <p className="text-base text-muted-foreground md:text-xl">{description}</p>
           </div>
         </div>
       </div>
